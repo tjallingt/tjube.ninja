3a053d315317f5372f74058234173c9188ed6000
tjallingt <tjallingt@gmail.com>
2015-04-03 14:47:10 +0200

    Updated readme and cleaned the node js server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # videoapp
-Video app in Node.JS
+Video app made with Node.JS
+
+A simple app that allows people to create a public screen on a beamer and control it with "remotes".
+Demo: http://app.tjallingt.com
+
+Inspired by http://protu.be/
--- video.js
+++ video.js
@@ -1,19 +1,24 @@
-var express = require( "express" );
-var app = express();
-var server = require( "http" ).Server( app );
-var io = require( "socket.io" )( server );
+/*
+	Node server for the videoapp.
+	The server generates a unique room id when a user connects and allows the remotes to send data to the screen(s).
+*/
+
+var express = require( "express" ),
+	app = express(),
+	server = require( "http" ).Server( app ),
+	io = require( "socket.io" )( server );
 
 server.listen( 1337 );
-//console.log( "server started on port 1337" );
+console.log( "server started on port 1337" );
 
-// Serve favicon with static
+// Serve static files
 app.use( express.static( __dirname + "/static" ) );
 
 // Show room select/create screen
 app.get( "/", function ( req, res ) {
 	var roomId = generateUniqueRoomId();
 	if( roomId ) res.redirect( "/room/" + roomId );
-	else res.status( 503 ).send( "Server might be full, please try again later :)" );
+	else res.status( 503 ).send( "Server is crowded, please try again later :)" );
 });
 
 // Show public screen
@@ -26,23 +31,23 @@ app.get( "/add/:room([a-z0-9]{3})", function ( req, res ) {
 	res.sendFile( __dirname + "/remote.html" );
 });
 
-// Communicate with clients 
+// Communicate with clients
 io.on( "connection", function ( socket ) {
 	socket.room = socket.request.headers.referer.replace( "http://"+socket.request.headers.host+"/", "" ).split("/")[1];
 	socket.join( socket.room );
-	//console.log( "a user connected to room "+socket.room );
+	console.log( "A user connected to room "+socket.room );
 	
 	socket.on( "cueVideo", function ( id ) {
 		socket.broadcast.to( socket.room ).emit( "cueVideo",  id );
 		socket.emit( "addedVideo",  id );
-		//console.log( "user added video "+id+" to room "+socket.room );
+		console.log( "User added video "+id+" to room "+socket.room );
 	});
 });
 
-// Tries 100 times to generate a unique room id of 3 characters
+// Tries 10 times to generate a unique room id of 3 characters
 function generateUniqueRoomId() {
 	var id = "";
-	for(var i = 0; i < 100; i++) {
+	for(var i = 0; i < 10; i++) {
 		var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
 		for( var i=0; i < 3; i++ ) id += possible.charAt(Math.floor(Math.random() * possible.length));
 		if( !io.sockets.adapter.rooms.hasOwnProperty( id ) ) return id;
